7270eb84aadf2cd5d4ae71c5b6f65187cd253751
Dasari venkatesh <54218123+Dasarivenkatesh@users.noreply.github.com>
2022-01-23 16:27:30 +0530

    Create diskspace.sh

---
+++ diskspace.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 60 ]; then
+    echo "Running out of 60 percentage space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
+     mail -s "Alert: Almost out of 60 percentage disk space $usep%" mb151897@gmail.com
+  else [ $usep -ge 60 ]; then
+     echo "Running out of 80 percentage space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
+    mail -s "Alert: Almost out of 81 percentage disk space $usep%" mb151897@gmail.comi
+  fi
+done
